91d14a5498b64e11635e1777005bcae0fb686726
SLFatemi <sinalotfifatemi@gmail.com>
2025-01-12 19:38:04 +0330

    Revert "add final(probably) log show"
    This reverts commit 5e1dc137ff6c6b8f3994e5ed8bf77448477a5810.

--- showlog.py
+++ showlog.py
@@ -3,8 +3,6 @@ import rich
 import subprocess
 import json
 
-from generalDefs import loading
-
 methods.clear()
 methods.printLine()
 
@@ -21,8 +19,3 @@ for game in logs:
     rich.print(f"    Game was played on: {game['date']}\n")
     # TODO CONVERT SECOND TO MINUTES AND HOUR
     rich.print(f"\t     Game took {game['length']} second(s)\n")
-rich.print(f"[orange1][bold]==============================================")
-rich.print("\n        [bright_white][bold]Enter anything to get back to [deep_pink4]menu[/deep_pink4]\n")
-s = input()
-loading()
-subprocess.run(["python", "menu.py"], check=True)
