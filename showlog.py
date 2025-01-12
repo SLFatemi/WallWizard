126f127a70c3abeb8fd8911b745efcd020605cd3
SLFatemi <sinalotfifatemi@gmail.com>
2025-01-12 19:38:07 +0330

    Revert "add better show log"
    This reverts commit 7b978b27fb96b2e1ced751d420131e7b0586fa25.

--- showlog.py
+++ showlog.py
@@ -15,7 +15,6 @@ with open("manageUsers/gamelog.json", 'r') as file:
 for game in logs:
     rich.print(f"[orange1][bold]============= GAME ID : [white]{game['id']}[/white] =============\n")
     rich.print(f"Player1 : [bright_red]{game['player1']}[/bright_red]\t\t\tPlayer2 : [bright_blue]{game['player2']}\n")
-    rich.print(f"\t      [gold1]W I N N E R ![/gold1] : {game['winner']}\n")
+    rich.print(f"\t     [gold1]W I N N E R ![/gold1] : {game['winner']}\n")
     rich.print(f"    Game was played on: {game['date']}\n")
-    # TODO CONVERT SECOND TO MINUTES AND HOUR
-    rich.print(f"\t     Game took {game['length']} second(s)\n")
+    rich.print(f"\tGame was going on for {game['length']} second(s)\n")
