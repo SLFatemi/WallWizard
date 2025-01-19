7485407b25f129f92a507286996febe21c973958
amir <manamir4179@gmail.com>
2025-01-18 20:48:30 -0800

    chore : convert seconds into hours

--- timer.py
+++ timer.py
@@ -1,12 +1,17 @@
 import time
+
 running = True
 secondes = 0
-end = 10 
+end = 8
 while (running):
     print(secondes)
     time.sleep(1)
     secondes += 1
+
     if(secondes >= end):
         running = False
         print(secondes)
+
 print("Time is UP!")
+# this is a simple timer for the game altough it was not predicted
+# so we do not need it until the game pushes a matter of time
